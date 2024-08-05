a0b9d6f15572e4215b7ed91789e8f28dd4cfb532
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-05 10:01:35 -0500

    removed functions had wrong file name to compile

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
 #ifndef _SORT_H_
 #define _SORT_H_
-
-
+#include <stddef.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,36 +21,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
-
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
 #endif
