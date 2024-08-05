455cef6bf7aaac184235f4933729624e62d82881
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-05 10:30:50 -0500

    recursion from function in header to swap nodes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,28 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - uses singly linked list to go through array
+ * @list: SLL in header
+ * Return: void,
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *now, *after, *temp;
 
+    if (list == NULL || *list == NULL || (*list)->after == NULL)
+    {
+        return;
+    }
+    now = (*list)->after;
+    while (now != NULL)
+    {
+        after = now->after;
+        temp = now->prev;
+        while ( temp != NULL && now->n < temp->n)
+        {
+            swap_list(list, temp, now);
+            print_list(*list);
+            temp = now->prev;
+        }
+        now = after;
+    }
+}
