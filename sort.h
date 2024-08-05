2ef41eac814098e649d9ab3965ee428e5d3dfffe
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-05 09:55:26 -0500

    added print functions in header...almost impossible to please betty with these assignments

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef _SORT_H_
+#define _SORT_H_
 
 
 
@@ -21,7 +21,36 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
+
+void print_list(const listint_t *list)
+{
+    int i;
+
+    i = 0;
+    while (list)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", list->n);
+        ++i;
+        list = list->next;
+    }
+    printf("\n");
+}
+
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
 
 #endif
