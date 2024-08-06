e0503b70cf1daefa25aa41dabb5a51b0cd342b08
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-05 23:18:03 -0500

    moved curly braces over

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,17 +2,17 @@
 void swap_list( listint_t **list, listint_t *N1, listint_t *N2)
 {
     if (N1->prev)
-    {
+{
         N1->prev->next = N2;
-    }
+}
     else
-    {
+{
         *list = N2;
-    }
+}
     if (N2->next)
-        {
+{
             N2->next->prev = N1;
-        }
+}
     N1->next = N2->next;
     N2->prev = N1->prev;
     N1->prev = N2;
@@ -28,20 +28,20 @@ void insertion_sort_list(listint_t **list)
     listint_t *now, *next, *temp;
 
     if (list == NULL || *list == NULL || (*list)->next == NULL)
-    {
+{
         return;
-    }
+}
     now = (*list)->next;
     while (now != NULL)
-    {
+{
         next = now->next;
         temp = now->prev;
         while ( temp != NULL && now->n < temp->n)
-        {
+{
             swap_list(list, temp, now);
             print_list(*list);
             temp = now->prev;
-        }
+}
         now = next;
-    }
+}
 }
