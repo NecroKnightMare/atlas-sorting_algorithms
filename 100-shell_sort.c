d190b76caeee8ead654f0dce3d78e47673a8db46
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-06-06 22:21:08 +0000

    added pseudocode into files. Next need to arrange them to fit what I need.

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -5,4 +5,27 @@
 /*use [gcc -Wall -Wextra -Werror -pedantic 100-main.c 100-shell_sort.c print_array.c -o shell]
 to compile and ./shell to test output*/
 
-void shell_sort(int *array, size_t size)
+void shell_sort(int *array, size_t size){
+    # Sort an array a[0...n-1].
+gaps = [701, 301, 132, 57, 23, 10, 4, 1]  # Ciura gap sequence
+
+# Start with the largest gap and work down to a gap of 1
+# similar to insertion sort but instead of 1, gap is being used in each step
+foreach (gap in gaps)
+{
+    # Do a gapped insertion sort for every element in gaps
+    # Each loop leaves a[0..gap-1] in gapped order
+    for (i = gap; i < n; i += 1)
+    {
+        # save a[i] in temp and make a hole at position i
+        temp = a[i]
+        # shift earlier gap-sorted elements up until the correct location for a[i] is found
+        for (j = i; (j >= gap) && (a[j - gap] > temp); j -= gap)
+        {
+            a[j] = a[j - gap]
+        }
+        # put temp (the original a[i]) in its correct location
+        a[j] = temp
+    }
+}
+}
--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -5,3 +5,21 @@
 print_array.c -o counting] to compile and ./counting to test*/
 
 void counting_sort(int *array, size_t size)
+{
+        count ← array of k + 1 zeros
+    output ← array of same length as input
+    
+    for i = 0 to length(input) - 1 do
+        j = key(input[i])
+        count[j] = count[j] + 1
+
+    for i = 1 to k do
+        count[i] = count[i] + count[i - 1]
+
+    for i = length(input) - 1 down to 0 do
+        j = key(input[i])
+        count[j] = count[j] - 1
+        output[count[j]] = input[i]
+
+    return output
+}
