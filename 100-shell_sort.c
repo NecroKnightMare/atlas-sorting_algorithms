3caf8d70e642fe10e40f49e48a979dee96a9a2e3
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-06-10 23:37:23 +0000

    not same output but gonna push in intranet

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,30 +2,39 @@
 #include <stdlib.h>
 #include "sort.h"
 
-/*use [gcc -Wall -Wextra -Werror -pedantic 100-main.c 100-shell_sort.c print_array.c -o shell]
+/*use [gcc -Wall -Wextra -Werror -pedantic 100-main.c 100-shell_sort.c
+print_array.c -o shell]
 to compile and ./shell to test output*/
 
-void shell_sort(int *array, size_t size){
-    # Sort an array a[0...n-1].
-gaps = [701, 301, 132, 57, 23, 10, 4, 1]  # Ciura gap sequence
-
-# Start with the largest gap and work down to a gap of 1
-# similar to insertion sort but instead of 1, gap is being used in each step
-foreach (gap in gaps)
+void shell_sort(int *array, size_t size)
 {
-    # Do a gapped insertion sort for every element in gaps
-    # Each loop leaves a[0..gap-1] in gapped order
-    for (i = gap; i < n; i += 1)
+    /*Sort an array a[0...n-1].*/
+    int gaps[] = {701, 301, 132, 57, 23, 10, 4, 1}; /*Ciura gap sequence*/
+    size_t num_gaps = sizeof(gaps) / sizeof(gaps[0]);
+
+    for (size_t g = 0; g < num_gaps; g++)
     {
-        # save a[i] in temp and make a hole at position i
-        temp = a[i]
-        # shift earlier gap-sorted elements up until the correct location for a[i] is found
-        for (j = i; (j >= gap) && (a[j - gap] > temp); j -= gap)
+        int gap = gaps[g];
+
+    /*Start with the largest gap and work down to a gap of 1*/
+    /*similar to insertion sort but instead of 1, gap is being used
+    in each step foreach (gap in gaps)*/
+    /*Do a gapped insertion sort for every element in gaps
+    Each loop leaves a[0..gap-1] in gapped order*/
+        for (size_t i = gap; i < size; i++)
         {
-            a[j] = a[j - gap]
-        }
-        # put temp (the original a[i]) in its correct location
-        a[j] = temp
+            /*save a[i] in temp and make a hole at position i*/
+            int temp = array[i];
+            size_t j = i;
+            /*shift earlier gap-sorted elements up until the correct
+            location for a[i] is found*/
+            while (j >= (size_t)gap && array[j - gap] > temp)
+            {
+                array[j] = array[j - gap];
+                j -= gap;
+            }
+            /*put temp (the original a[i]) in its correct location*/
+            array[j] = temp;
+        }    
     }
-}
 }
