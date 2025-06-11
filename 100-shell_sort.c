b8285b31d015a65d721167e4bb94997076867bdc
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-06-11 00:11:46 +0000

    Betty doc

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,19 +2,24 @@
 #include <stdlib.h>
 #include "sort.h"
 
-/*use [gcc -Wall -Wextra -Werror -pedantic 100-main.c 100-shell_sort.c
-print_array.c -o shell]
-to compile and ./shell to test output*/
+/*
+*use [gcc -Wall -Wextra -Werror -pedantic 100-main.c 100-shell_sort.c
+*print_array.c -o shell]
+*to compile and ./shell to test output
+*/
 void print_array(const int *array, size_t size);
 
 void shell_sort(int *array, size_t size)
 {
 	/*Generate Knuth sequence dynamically*/
 	int gap = 1;
-	size_t i, j ;
+	size_t i, j;
 
 	/*Error handling*/
-	if (!array || size < 2) return;
+	if (!array || size < 2)
+	{
+		return;
+	}
 
 	while (gap < (int)size / 3)
 	{
@@ -22,11 +27,12 @@ void shell_sort(int *array, size_t size)
 	}
 
 	while (gap >= 1)
-    {
+	{
 		for (i = gap; i < size; i++)
 		{
 			int temp = array[i];
 			j = i;
+
 
 			while (j >= (size_t)gap && array[j - gap] > temp)
 			{
