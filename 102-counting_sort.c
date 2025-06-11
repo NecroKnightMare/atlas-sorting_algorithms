cbf42928ed3e4daec19fcc04da1ce1891022ff5c
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-06-11 00:38:04 +0000

    gonna see what the checker says, code isnt outputting what it shoul

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,25 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-/*use [gcc -Wall -Wextra -Werror -pedantic 102-main.c 102-counting_sort.c 
-print_array.c -o counting] to compile and ./counting to test*/
+/*
+use [gcc -Wall -Wextra -Werror -pedantic 102-main.c 102-counting_sort.c 
+print_array.c -o counting] to compile
+and ./counting to test
+*/
+void print_array(const int *array, size_t size);
 
 void counting_sort(int *array, size_t size)
 {
-        count ← array of k + 1 zeros
-    output ← array of same length as input
+    if (!array || size < 2)
+    {
+        return;
+    }
+    int max = array[0];
+    size_t i;
+
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    int *count = calloc(max + 1, sizeof(int));
+    if (!count)
+    {
+        return;
+    }
+    for ( i = 0; i< size; i++)
+    {
+    count[array[i]]++;
+    }
+
+    print_array(count, max +1);
+
+    for (i = 1; i <= (size_t)max; i++)
+    {
+        count[i] += count[i - 1];
+    }
+    int *sorted = malloc(size * sizeof(int));
+    if (!sorted)
+    {
+        free(count);
+        return;
+    }
     
-    for i = 0 to length(input) - 1 do
-        j = key(input[i])
-        count[j] = count[j] + 1
+    for (int i = (int)size - 1; i >= 0; i--)
+    {
+        sorted[count[array[i]] - 1] = array[i];
+        count[array[i]]--;
+    }
+    for (i = 0; i < size; i++)
+    {
+        array[i] = sorted[i];
+    }
 
-    for i = 1 to k do
-        count[i] = count[i] + count[i - 1]
-
-    for i = length(input) - 1 down to 0 do
-        j = key(input[i])
-        count[j] = count[j] - 1
-        output[count[j]] = input[i]
-
-    return output
+    free(sorted);
+    free(count);
 }
