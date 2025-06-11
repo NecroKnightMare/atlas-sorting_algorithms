c3bdf3935566689ba36bbba1c7f466551e131e3a
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-06-11 00:06:55 +0000

    changed from spaces to tab--Betty Doc

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,34 +9,34 @@ void print_array(const int *array, size_t size);
 
 void shell_sort(int *array, size_t size)
 {
-    /*Generate Knuth sequence dynamically*/
-    int gap = 1;
-    size_t i, j ;
-    
-    /*Error handling*/
-    if (!array || size < 2) return;
+	/*Generate Knuth sequence dynamically*/
+	int gap = 1;
+	size_t i, j ;
 
-    while (gap < (int)size / 3) {
-        gap = gap * 3 + 1; /*Knuth formula*/
-    }
+	/*Error handling*/
+	if (!array || size < 2) return;
 
-    while (gap >= 1)
+	while (gap < (int)size / 3)
+	{
+		gap = gap * 3 + 1; /*Knuth formula*/
+	}
+
+	while (gap >= 1)
     {
-        
-        for (i = gap; i < size; i++)
-        {
-            int temp = array[i];
-            j = i;
+		for (i = gap; i < size; i++)
+		{
+			int temp = array[i];
+			j = i;
 
-            while (j >= (size_t)gap && array[j - gap] > temp)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
-            array[j] = temp;
-        }
-        print_array(array, size);
+			while (j >= (size_t)gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
 
-        gap /= 3; /*Reduce gap following Knuth sequence*/
-    }
+		gap /= 3; /*Reduce gap following Knuth sequence*/
+	}
 }
